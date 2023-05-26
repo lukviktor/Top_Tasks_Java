267a8532383e45285898f4eb8347a6884afc5b98
Viktor Lukashev <lukashevviktor@gmail.com>
2023-05-26 19:56:29 +0300

    Решаем задачи остановились на 8

---
+++ src/test/java/Top20TasksTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Top20TasksTest {
+
+    @Test
+    public void flipOverCycleForMethod2Test() {
+
+    }
+}
